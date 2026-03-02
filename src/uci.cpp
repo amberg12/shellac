1fb08c1ae9e885473dbba77c7af5fbefc29624f8
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-03-02 00:46:54 +0000

    initial commit

---
+++ src/uci.cpp
@@ -0,0 +1,36 @@
+/*
+  Shellac - A UCI chess engine.
+  Copyright (C) 2026 Amber Goulding
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "uci.h"
+
+namespace shellac {
+void UciEngine::loop()
+{
+    std::string token, line;
+    while (token != "quit") {
+        std::getline(std::cin, line);
+        std::istringstream iss{line};
+
+        iss >> token;
+    }
+}
+} // namespace shellac
