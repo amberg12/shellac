efa51b65e24db68c04acb5dac647282e86058f84
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-03-09 00:34:30 +0000

    add psqt

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -16,53 +16,181 @@
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+// Piece-Square tables taken from Simplified Evaluation Function (CPW)
+
 #include "evaluate.h"
 #include "position.h"
 #include "types.h"
 
 namespace shellac {
 namespace {
+using Psqt = std::array<Score, 64>;
+
 Score evaluate_pawns(const Position& position)
 {
-    const Score whitePawns = position.pieces(Color::WHITE, PieceType::PAWN).pop_count() * Material::PAWN_SCORE;
-    const Score blackPawns = position.pieces(Color::BLACK, PieceType::PAWN).pop_count() * Material::PAWN_SCORE;
-    return whitePawns - blackPawns;
+    // clang-format off
+    constexpr Psqt PAWN_TABLE = {
+        0,   0,   0,   0,   0,   0,   0,   0,
+        5,  10,  10, -20, -20,  10,  10,   5,
+        5,  -5, -10,   0,   0, -10,  -5,   5,
+        0,   0,   0,  20,  20,   0,   0,   0,
+        5,   5,  10,  25,  25,  10,   5,   5,
+       10,  10,  20,  30,  30,  20,  10,  10,
+       50,  50,  50,  50,  50,  50,  50,  50,
+        0,   0,   0,   0,   0,   0,   0,   0
+    };
+    //clang-format on
+    Score ret = 0;
+    for (const Square square : position.pieces(Color::WHITE, PieceType::PAWN)) {
+        ret += PAWN_SCORE;
+        ret += PAWN_TABLE[underlying(square)];
+    }
+
+    for (const Square square : position.pieces(Color::BLACK, PieceType::PAWN)) {
+        ret -= PAWN_SCORE;
+        ret -= PAWN_TABLE[underlying(mirror(square))];
+    }
+
+    return ret;
 }
 
 Score evaluate_knights(const Position& position)
 {
-    const Score whiteKnights =
-        position.pieces(Color::WHITE, PieceType::KNIGHT).pop_count() * Material::KNIGHT_SCORE;
-    const Score blackKnights =
-        position.pieces(Color::BLACK, PieceType::KNIGHT).pop_count() * Material::KNIGHT_SCORE;
-    return whiteKnights - blackKnights;
+    // clang-format off
+    constexpr Psqt KNIGHT_TABLE = {
+        -50, -40, -30, -30, -30, -30, -40, -50,
+        -40, -20,   0,   5,   5,   0, -20, -40,
+        -30,   5,  10,  15,  15,  10,   5, -30,
+        -30,   0,  15,  20,  20,  15,   0, -30,
+        -30,   5,  15,  20,  20,  15,   5, -30,
+        -30,   0,  10,  15,  15,  10,   0, -30,
+        -40, -20,   0,   0,   0,   0, -20, -40,
+        -50, -40, -30, -30, -30, -30, -40, -50
+     };
+    // clang-format on
+
+    Score ret = 0;
+
+    for (const Square square : position.pieces(Color::WHITE, PieceType::KNIGHT)) {
+        ret += KNIGHT_SCORE;
+        ret += KNIGHT_TABLE[underlying(square)];
+    }
+
+    for (const Square square : position.pieces(Color::BLACK, PieceType::KNIGHT)) {
+        ret -= KNIGHT_SCORE;
+        ret -= KNIGHT_TABLE[underlying(mirror(square))];
+    }
+
+    return ret;
 }
 
 Score evaluate_bishops(const Position& position)
 {
-    const Score whiteBishops =
-        position.pieces(Color::WHITE, PieceType::BISHOP).pop_count() * Material::BISHOP_SCORE;
-    const Score blackBishops =
-        position.pieces(Color::BLACK, PieceType::BISHOP).pop_count() * Material::BISHOP_SCORE;
-    return whiteBishops - blackBishops;
+    // clang-format off
+    constexpr Psqt BISHOP_TABLE = {
+        -20, -10, -10, -10, -10, -10, -10, -20,
+        -10,   5,   0,   0,   0,   0,   5, -10,
+        -10,  10,  10,  10,  10,  10,  10, -10,
+        -10,   0,  10,  10,  10,  10,   0, -10,
+        -10,   5,   5,  10,  10,   5,   5, -10,
+        -10,   0,   5,  10,  10,   5,   0, -10,
+        -10,   0,   0,   0,   0,   0,   0, -10,
+        -20, -10, -10, -10, -10, -10, -10, -20
+     };
+    // clang-format on
+
+    Score ret = 0;
+
+    for (const Square square : position.pieces(Color::WHITE, PieceType::BISHOP)) {
+        ret += BISHOP_SCORE;
+        ret += BISHOP_TABLE[underlying(square)];
+    }
+
+    for (const Square square : position.pieces(Color::BLACK, PieceType::BISHOP)) {
+        ret -= BISHOP_SCORE;
+        ret -= BISHOP_TABLE[underlying(mirror(square))];
+    }
+
+    return ret;
 }
 
 Score evaluate_rooks(const Position& position)
 {
-    const Score whiteRooks =
-        position.pieces(Color::WHITE, PieceType::ROOK).pop_count() * Material::ROOK_SCORE;
-    const Score blackRooks =
-        position.pieces(Color::BLACK, PieceType::ROOK).pop_count() * Material::ROOK_SCORE;
-    return whiteRooks - blackRooks;
+    // clang-format off
+    constexpr Psqt ROOK_TABLE = {
+        0,   0,   0,   5,   5,   0,   0,   0,
+       -5,   0,   0,   0,   0,   0,   0,  -5,
+       -5,   0,   0,   0,   0,   0,   0,  -5,
+       -5,   0,   0,   0,   0,   0,   0,  -5,
+       -5,   0,   0,   0,   0,   0,   0,  -5,
+       -5,   0,   0,   0,   0,   0,   0,  -5,
+        5,  10,  10,  10,  10,  10,  10,   5,
+        0,   0,   0,   0,   0,   0,   0,   0
+   };
+    // clang-format on
+
+    Score ret = 0;
+
+    for (const Square square : position.pieces(Color::WHITE, PieceType::ROOK)) {
+        ret += ROOK_SCORE;
+        ret += ROOK_TABLE[underlying(square)];
+    }
+
+    for (const Square square : position.pieces(Color::BLACK, PieceType::ROOK)) {
+        ret -= ROOK_SCORE;
+        ret -= ROOK_TABLE[underlying(mirror(square))];
+    }
+
+    return ret;
 }
 
 Score evaluate_queens(const Position& position)
 {
-    const Score whiteQueens =
-        position.pieces(Color::WHITE, PieceType::QUEEN).pop_count() * Material::QUEEN_SCORE;
-    const Score blackQueens =
-        position.pieces(Color::BLACK, PieceType::QUEEN).pop_count() * Material::QUEEN_SCORE;
-    return whiteQueens - blackQueens;
+    // clang-format off
+    constexpr Psqt QUEEN_TABLE = {
+        -20, -10, -10,  -5,  -5, -10, -10, -20,
+        -10,   0,   5,   0,   0,   5,   0, -10,
+        -10,   5,   5,   5,   5,   5,   5, -10,
+          0,   0,   5,   5,   5,   5,   0,  -5,
+         -5,   0,   5,   5,   5,   5,   0,  -5,
+        -10,   0,   5,   5,   5,   5,   0, -10,
+        -10,   0,   0,   0,   0,   0,   0, -10,
+        -20, -10, -10,  -5,  -5, -10, -10, -20
+     };
+    // clang-format on
+
+    Score ret = 0;
+
+    for (const Square square : position.pieces(Color::WHITE, PieceType::QUEEN)) {
+        ret += QUEEN_SCORE;
+        ret += QUEEN_TABLE[underlying(square)];
+    }
+
+    for (const Square square : position.pieces(Color::BLACK, PieceType::QUEEN)) {
+        ret -= QUEEN_SCORE;
+        ret -= QUEEN_TABLE[underlying(mirror(square))];
+    }
+
+    return ret;
+}
+
+Score evaluate_kings(const Position& position)
+{
+    constexpr Score KING_TABLE[64] = {
+        20,  30,  10,   0,   0,  10,  30,  20,
+        20,  20,   0,   0,   0,   0,  20,  20,
+       -10, -20, -20, -20, -20, -20, -20, -10,
+       -20, -30, -30, -40, -40, -30, -30, -20,
+       -30, -40, -40, -50, -50, -40, -40, -30,
+       -30, -40, -40, -50, -50, -40, -40, -30,
+       -30, -40, -40, -50, -50, -40, -40, -30,
+       -30, -40, -40, -50, -50, -40, -40, -30
+    };
+    // clang-format on
+    Score ret = 0;
+    ret += KING_TABLE[underlying(position.king_square(Color::WHITE))];
+    ret -= KING_TABLE[underlying(mirror(position.king_square(Color::BLACK)))];
+    return ret;
 }
 }
 
@@ -75,7 +203,7 @@ Score evaluate(const Position& position)
     score += evaluate_bishops(position);
     score += evaluate_rooks(position);
     score += evaluate_queens(position);
+    score += evaluate_kings(position);
 
-    return position.side_to_move() == Color::WHITE ? score : -score;
-}
+    return (position.side_to_move() == Color::WHITE) ? score : -score;}
 } // namespace shellac
